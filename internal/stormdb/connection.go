b4735536252920963d41b40e1864bcff53a57e78
abbing <lreg0@web.de>
2025-06-12 11:23:09 +0200

    fix db location bug

--- internal/stormdb/connection.go
+++ internal/stormdb/connection.go
@@ -3,12 +3,20 @@ package stormdb
 import (
 	"github.com/asdine/storm/v3"
 	log "github.com/sirupsen/logrus"
+	"os"
+	"path/filepath"
 )
 
 var DB *storm.DB
 
 func ConnectToDB() {
-	db, err := storm.Open("./timetracker.db")
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Fatal("Fehler beim Ermitteln des Binary-Pfads:", err)
+		return
+	}
+	exeDir := filepath.Dir(exePath)
+	db, err := storm.Open(exeDir + "/timetracker.db")
 	if err != nil {
 		log.Fatal("Could not connect to database", err)
 	}
