c2645feb47b96495ced7f167cb4d7715d8194df3
abbing <lreg0@web.de>
2025-06-12 09:49:44 +0200

    finish up ui

--- internal/api/api-webserver.go
+++ internal/api/api-webserver.go
@@ -2,35 +2,50 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/LReg/time-tracker/internal/timeslot"
 	"github.com/LReg/time-tracker/internal/util"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
 
 func StartTimeSlotApi() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/quit", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		writer.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		writer.WriteHeader(http.StatusOK)
+		_, err := writer.Write([]byte("OK"))
+		if err != nil {
+			log.Error("Error writing response", err)
+		}
+		go func() {
+			time.Sleep(1 * time.Second)
+			os.Exit(0)
+		}()
+	})
 	mux.HandleFunc("/tt/{ms}", func(writer http.ResponseWriter, request *http.Request) {
 		ms := request.PathValue("ms")
 		millis, err := strconv.ParseInt(ms, 10, 64)
 		if err != nil {
 			log.Error("Error converting ms to int", err)
 		}
-		fmt.Println("millis", millis)
 
 		date := time.UnixMilli(millis)
 		from := util.GetLastMidnightFrom(date)
 		to := util.GetLastMidnightFrom(date.Add(24 * time.Hour))
 
-		fmt.Println("from", from, "to", to)
-
 		timeslots := timeslot.GetTimeSlotsFromTo(from, to)
+		writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		writer.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-		fmt.Println("timeslots", timeslots)
 		if len(timeslots) == 0 {
+			writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
 			writer.WriteHeader(http.StatusNotFound)
 			writer.Write([]byte("Not Found"))
 			return
@@ -39,15 +54,13 @@ func StartTimeSlotApi() {
 		jsonTimeslots, err := json.Marshal(timeslots)
 		if err != nil {
 			log.Error("Error converting timeslot to json", err)
+			writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
 			writer.WriteHeader(http.StatusInternalServerError)
 			writer.Write([]byte("Error converting timeslot to json"))
 			return
 		}
 
 		writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-		writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
-		writer.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
-		writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		_, err = writer.Write(jsonTimeslots)
 		if err != nil {
--- internal/ui/ui-webserver.go
+++ internal/ui/ui-webserver.go
@@ -1,14 +1,29 @@
 package ui
 
 import (
+	"embed"
+	"github.com/LReg/time-tracker/internal/util"
 	log "github.com/sirupsen/logrus"
+	"io/fs"
 	"net/http"
+	"time"
 )
 
+//go:embed web/*
+var uiFiles embed.FS
+
 func StartStaticWebServer() {
+	subFS, err := fs.Sub(uiFiles, "web")
+	if err != nil {
+		log.Fatal(err)
+	}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("../web")))
-	err := http.ListenAndServe(":4200", mux)
+	mux.Handle("/", http.FileServer(http.FS(subFS)))
+	go func() {
+		time.Sleep(1 * time.Second)
+		util.OpenBrowser("http://localhost:4200")
+	}()
+	err = http.ListenAndServe(":4200", mux)
 	if err != nil {
 		log.Fatal("Could not start webserver", err)
 	}
